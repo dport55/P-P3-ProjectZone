1f0014ad9403f97a845abf6c66070cf22c3efec9
Amata Allen <colorclouds321@gmail.com>
2025-03-16 00:47:42 -0400

    Created the menus
    Pause, Win, and Lose menus

--- Project Zero/Assets/Scenes/Delvin's Scene/Scripts/GameManager.cs
+++ Project Zero/Assets/Scenes/Delvin's Scene/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance;
+    [SerializeField] GameObject menuActive, menuPause, menuWin, menuLose;
+    public bool isPaused;
     public GameObject player;
     public PlayerController playerScript;
  
@@ -16,15 +18,73 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         instance = this;
-        player = GameObject.FindWithTag("Player");
-        playerScript = player.GetComponent<PlayerController>();
+        //player = GameObject.FindWithTag("Player");
+        //playerScript = player.GetComponent<PlayerController>();
    
+    }
+
+    private void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        if (player != null)
+        {
+            playerScript = player.GetComponent<PlayerController>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
     }
 
-  
+    public void ShowWinMenu()
+    {
+        statePause();
+        menuActive = menuWin;
+        menuActive.SetActive(true);
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+
+
 }
