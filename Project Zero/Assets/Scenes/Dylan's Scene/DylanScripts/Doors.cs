76a267bd4c196622485ea1fcdbd0a7155130a37e
dport55 <drporterfield@student.fullsail.edu>
2025-03-20 21:35:38 -0400

    Level mods

--- Project Zero/Assets/Scenes/Dylan's Scene/DylanScripts/Doors.cs
+++ Project Zero/Assets/Scenes/Dylan's Scene/DylanScripts/Doors.cs
@@ -119,7 +119,10 @@ public class DoorTrigger : MonoBehaviour
                     isOpening = true;
                     StartCoroutine(OpenDoor());
                 }
-                doorPrompt.SetActive(true);
+                else
+                {
+                    doorPrompt.SetActive(true);
+                }
             }
         }
     }
