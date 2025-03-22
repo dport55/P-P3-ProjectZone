3d9a142ffdd53e752a5dea4c5b1345e7cd3b8c71
Amata Allen <colorclouds321@gmail.com>
2025-03-22 16:54:02 -0400

    Fixed main menu buttons and put the pause menu background in

--- Project Zero/Assets/Scenes/Amata's Scene/Scripts/MainMenuManager.cs
+++ Project Zero/Assets/Scenes/Amata's Scene/Scripts/MainMenuManager.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.ProBuilder.MeshOperations;
+
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] public GameObject menuActive, menuPause, menuWin, menuLose, menuTutorial, retical, PlayButton;
+    public static MainMenuManager instance;
+    public bool isPaused;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
